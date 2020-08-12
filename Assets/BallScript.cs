b8d8ca00a051697728f038773c6277bfea58049b
hubfil <faleev2@yandex.ru>
2020-08-12 17:04:24 +0300

    17:00

---
+++ Assets/BallScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    public Vector3 startPosition;
+    public Vector3 startForceV3;
+    public float startForce;
+    public float desiredSpeed;
+    public Rigidbody rb;
+    [Header("защита от застреваний")]
+    public float timeToReturn;
+    public float timeToReturnMax;
+
+
+    void Start()
+    {
+        startPosition = transform.position;
+        StartBall();
+    }
+
+    public void StartBall()
+    {
+        Debug.Log(gameObject.name + " запущен");
+        rb.isKinematic = true;
+        rb.velocity = (startForceV3 * startForce);
+        rb.isKinematic = false;
+
+    }
+
+    public void FixedUpdate()
+    {
+        UdjustSpeed();
+    }
+
+    private void Update()
+    {
+        //защита от застреваний
+        timeToReturn += Time.deltaTime;
+        if (timeToReturn > timeToReturnMax)
+            rb.useGravity = true;
+    }
+
+    public void UdjustSpeed()
+    {
+        //если слишком быстро или медленно летит
+        float adjustCoef;
+        adjustCoef =  desiredSpeed / rb.velocity.magnitude;
+        rb.velocity = rb.velocity * adjustCoef;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "brick")
+        {
+            Destroy(collision.gameObject, 0.1f);
+        }
+        if (collision.transform.tag == "player")
+        {
+            timeToReturn = 0;
+            rb.useGravity = false;
+        }
+
+
+        }
+
+    }
---
+++ Assets/playerRotatorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerRotatorScript : MonoBehaviour
+{
+
+    public bool isLeft;
+    public float rotateAmount;
+    public GameObject playerObj;
+    public Quaternion startRotation;
+    public float duration;
+
+
+    private void Start()
+    {
+        playerObj = gameObject.transform.parent.gameObject;
+        startRotation = playerObj.transform.rotation;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ball")
+        {
+            Debug.Log("поворот");
+            if (isLeft)
+                playerObj.transform.Rotate(0, 0, - rotateAmount );
+            if (!isLeft)
+                playerObj.transform.Rotate(0, 0, rotateAmount);
+            StartCoroutine(returnRotation());
+        }
+    }
+    IEnumerator returnRotation()
+    {
+        yield return new WaitForSeconds(duration);
+        Debug.Log("возвращение");
+        playerObj.transform.rotation = startRotation;
+
+    }
+}
